ac6fd955a14f7234b5770782996444a21eb510d3
yattom <tsutomu.yasui@gmail.com>
2023-02-03 10:57:51 +0000

    end of ch.2 2023/2/3

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -1,3 +1,4 @@
+
 package money;
 
 import org.junit.jupiter.api.Test;
@@ -7,9 +8,9 @@ public class MoneyTest {
   @Test
   public void testMultiplication() {
     Dollar five = new Dollar(5);
-    five.times(2);
-    assertEquals(10, five.amount); 
-    five.times(3);
-    assertEquals(15, five.amount);
+    Dollar product = five.times(2);
+    assertEquals(10, product.amount); 
+    product = five.times(3);
+    assertEquals(15, product.amount);
   }
 }
