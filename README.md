f4f4121a7e510c5da2c64dcb697ea48f751dd8b7
yattom <tsutomu.yasui@gmail.com>
2023-03-17 10:06:47 +0000

    delete all

--- README.md
+++ README.md
@@ -8,7 +8,34 @@
 
 packageを追加したときは、コンソールからmvn eclipse:eclipseを実行して、.projectと.classpathファイルを更新してください。そうしないと、コード補完などが効かない場合があります。
 
+## TODOリスト
+
+- [ ] $5 + 10CHF = $10 (レートが2:1の場合)
+- [x] $5 * 2 = $10
+- [x] amountをprivateにする
+- [x] Dollarの副作用どうする？
+- [ ] Moneyの丸め処理どうする？
+- [x] equals()
+- [ ] hashCode()
+- [ ] nullとの等価性比較
+- [ ] 他のオブジェクトとの等価性比較
+- [x] 5CHF * 2 = 10CHF
+- [ ] DollarとFrancの重複
+- [x] equalsの一般化
+- [ ] timesの一般化
+- [x] FrancとDollarを比較する
+- [x] 通貨の概念
+- [ ] testFrancMultiplicationを削除する？
+
 # モブの順番
+
+## 2023.3.3
+- 宮崎
+- えんじょうじ
+- くぼ
+- jin
+- terahide
+- 401
 
 ## 2023.2.17
 - 宮崎
--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,8 +1,23 @@
 package money;
-class Money {
+abstract class Money {
   protected int amount;
+  protected String currency;
+  Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+  abstract Money times(int multiplier);
+  String currency() {
+    return currency;
+  }
   public boolean equals(Object object) {
     Money money = (Money) object;
     return amount == money.amount && getClass().equals(money.getClass());
+  }
+  static Money dollar(int amount) {
+    return new Dollar(amount, "USD");
+  }
+  static Money franc(int amount) {
+    return new Franc(amount, "CHF");
   }
 }
