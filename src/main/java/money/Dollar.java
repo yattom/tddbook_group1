5eef859ac069d653d218eadeb66812ab21599fe8
yattom <tsutomu.yasui@gmail.com>
2024-03-01 21:54:14 +0000

    end of Ch.2

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -5,13 +5,7 @@ class Dollar {
   Dollar(int amount) {
     this.amount = amount;
   }
-
   Dollar times(int multiplier) {
     return new Dollar(amount * multiplier);
-  }
-
-  public boolean equals(Object object) {
-    Dollar dollar = (Dollar) object;
-    return amount == dollar.amount;
   }
 }
