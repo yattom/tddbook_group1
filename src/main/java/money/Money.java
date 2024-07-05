cc5683b33183e7426be44615cab84db0552f0c51
yattom <>
2024-07-05 10:06:33 +0000

    end of 2024.6.21

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -7,10 +7,10 @@ class Money implements Expression {
     this.currency = currency;
   } 
   protected int amount;
-  Money times(int multiplier){
+  Expression times(int multiplier){
     return new Money(amount * multiplier, currency);
   }
-  Expression plus(Money addend) {
+  public Expression plus(Expression addend) {
     return new Sum(this, addend);
   }
   public Money reduce(Bank bank, String to) {
